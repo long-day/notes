06f4d5fa683ac573e3f255995dd9e544c1999d03
longday <3226965859@qq.com>
2023-07-17 09:11:35 +0800

    spring-boot-deeper init

---
+++ spring-boot-deeper/spring-boot-deeper/src/main/java/me/longday/SpringBootDeeperApplication.java
@@ -0,0 +1,13 @@
+package me.longday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDeeperApplication.class, args);
+    }
+
+}
